7efca7ef431433172820726ea9ef1029028560d3
Mohan Manikanta <mohan.manikanta@hashicorp.com>
2025-04-30 17:13:37 +0530

    Test

--- dawdle_test.go
+++ dawdle_test.go
@@ -199,7 +199,7 @@ func TestProxy(t *testing.T) {
 	// want a size that is going to exhaust the proxy buffer, so we
 	// create a buffer of default size * 2.
 
-	writeBuffer := make([]byte, 1024*1024)
+	writeBuffer := make([]byte, 10*1024*1024)
 	var expectedB []byte
 
 	// ***********************
